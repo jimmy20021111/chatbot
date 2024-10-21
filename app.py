e8920334bb4fbcc35209567127791b3acd9ea413
jimmy <106481562+jimmy20021111@users.noreply.github.com>
2024-10-21 17:39:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,14 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage
 from langchain_ollama import OllamaLLM
 from langchain.prompts import ChatPromptTemplate
 
+import tempfile, os
+import datetime
+import openai
+import time
+
 # 初始化 LINE Bot API
-# LINE_CHANNEL_ACCESS_TOKEN = 'RBddbMauukL3lZ+ryEsnw91JVq0TJE+RB9+Q0zkplX15Otz2DE0qDcgasVF2vQj+ddb2ALFpDFYVmWflvnYtRWTkrubiSic4rAyBEu7YLnQQNJQ0I24JApAmBJUVO6ddu7VgZMi5XwqX2FYZdRmB/AdB04t89/1O/w1cDnyilFU='
-# LINE_CHANNEL_SECRET = 'e17f3f4bd01d523cfb56a2ce34084699'
+#LINE_CHANNEL_ACCESS_TOKEN = 'RBddbMauukL3lZ+ryEsnw91JVq0TJE+RB9+Q0zkplX15Otz2DE0qDcgasVF2vQj+ddb2ALFpDFYVmWflvnYtRWTkrubiSic4rAyBEu7YLnQQNJQ0I24JApAmBJUVO6ddu7VgZMi5XwqX2FYZdRmB/AdB04t89/1O/w1cDnyilFU='
+#LINE_CHANNEL_SECRET = 'e17f3f4bd01d523cfb56a2ce34084699'
 line_bot_api = LineBotApi('LINE_CHANNEL_ACCESS_TOKEN')
 handler = WebhookHandler('LINE_CHANNEL_SECRET')
 
@@ -26,21 +31,15 @@ model = OllamaLLM(model="llama3")
 prompt = ChatPromptTemplate.from_template(template)
 chain = prompt | model
 
-app = Flask(__name__)
 conversation_context = ""  # 存儲對話歷史
 
-@app.route("/callback", methods=['POST'])
-def callback():
-    # 獲取 LINE Webhook 的請求內容
-    signature = request.headers['X-Line-Signature']
-    body = request.get_data(as_text=True)
-    
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
+app = Flask(__name__)
+static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
+# Channel Access Token
+line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
+# Channel Secret
+handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
 
-    return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
@@ -67,4 +66,5 @@ def handle_message(event):
         )
 
 if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
     app.run(host="0.0.0.0", port=8000)
